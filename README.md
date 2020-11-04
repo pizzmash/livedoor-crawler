e7b30319ec41f268338ef5e32e08f2b78b54af90
Yuma Suzuki <y.suzuki@mibel.cs.tsukuba.ac.jp>
2020-11-04 23:55:19 +0900

    argparseの導入！

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 SeleniumとChromeDriverでlivedoorNEWSから記事を持ってきてcsvに出力するやつ
 
 ## Usage
+output.csvに出力
 ```
-python main.py
+python main.py output.csv
+```
+dockerを使っている場合
+```
+python main.py output.csv --docker
 ```
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import argparse
 from writer import Writer
 from crawler import Crawler
 
 
 def main():
-	writer = Writer('./test.csv')
-	crawler = Crawler(docker=True)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('out', help='output csv file')
+  parser.add_argument('--docker', action='store_true', help='if you are using docker')
+  args = parser.parse_args()
 
-	for i, article in enumerate(crawler.crawl(sleep_time=5)):
-		writer.write(article)
-		print("csvに書き込みました({}件完了)\n".format(i+1))
-	f.close()
+  writer = Writer(args.out)
+  crawler = Crawler(docker=args.docker)
+
+  for i, article in enumerate(crawler.crawl(sleep_time=5)):
+    writer.write(article)
+    print("csvに書き込みました({}件完了)\n".format(i+1))
+  f.close()
 
 
 if __name__ == "__main__":
-	main()
+  main()
